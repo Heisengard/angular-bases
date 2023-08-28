b1ca21c9488e45576e0277760421bdddf6494f2b
Richard <richardramirez91@gmail.com>
2023-08-27 19:02:51 -0500

    Actualizado

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
+import { Character } from './../interfaces/character.interface';
 import { Component, OnInit } from '@angular/core';
-import { Character } from '../interfaces/character.interface';
+import { dbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -8,14 +9,23 @@ import { Character } from '../interfaces/character.interface';
 
 export class MainPageComponent {
 
-  public characters: Character[]= [{
-    name: 'Krillin',
-    power: 1000
-  },{
-    name: 'Goku',
-    power: 9500
-  }
-];
+  constructor( private dbzService: dbzService) { }
 
-  ngOnInit() { }
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+
+  }
+
+  onDeleteCharacter(id: string): void{
+    this.dbzService.deleteCharacterById(id);
+
+  }
+
+  onNewCharacter(character: Character):void {
+    this.dbzService.addCharacter(character);
+
+  }
+
+
+
 }
